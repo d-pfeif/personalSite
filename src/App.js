e8980fcf699f8d89c41442348bb9c54d2c410f01
Dakota J Pfeifer <dakota.pfeifer@yahoo.com>
2018-02-11 22:04:46 -0700

    added about me section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
-import logo from "./assets/logo.png"
 import './App.css';
+import AboutMe from "./components/aboutMe.js"
+import Landing from "./components/landing.js"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      logoWidth: 50
+    }
+
+    this.navBarChange = this.navBarChange.bind(this)
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.navBarChange)
+  }
+
+  navBarChange() {
+    let Y = window.innerHeight / 1.75
+    let X = window.innerHeight
+    if (window.scrollY > Y) {
+      // if(window.scrollY) {
+      while (this.state.logoWidth > 20) {
+        // console.log('haha');
+        this.setState({logoWidth: this.state.logoWidth - 1})
+
+      }
+        // console.log(this.state.logoWidth);
+      // }
+    } else {
+      this.setState({logoWidth: 50})
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Landing logoWidth={this.state.logoWidth} />
+        <AboutMe />
       </div>
     );
   }
---
+++ src/components/aboutMe.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import myFace from '../assets/myFace.jpg'
+
+
+class AboutMe extends Component {
+  render(){
+    return(
+      <div className="about">
+        <img src={myFace} className="myFace" />
+        <div className="aboutSection">
+          <h2>ABOUT ME</h2>
+          <p>I was born in Minnesota and moved to Colorado when I was 11. When I turned 19 I decided to pursue a career in the medical field. After about 5 years I realized it wasn't really for me. There were some aspects of the job I really liked: team communication and time management. But I felt like I wasn't able to be as creative as I wanted to be and most of the problem solving I loved to do was spent on Calculus or sudoku.</p>
+          <p>I wanted a career change and I always had a sort of affinity for coding. When I wasn't working full time or going to school to get a biology degree, I would dabble in programming, but I never found the time to immerse myself in an industry that would provide everything I could ever want in a career: team communication, time management, creativity, problem solving, and continual learning!</p>
+          <p>It was at this point in my life I decided I was going to be a developer and if I was going to dive into a career, I would dive into the deep end. So I went to Galvanize and graduated their six month program.</p>
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default AboutMe
