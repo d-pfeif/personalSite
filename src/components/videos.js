d3b94cfa3fd17cd450c66c1313f2e982b355773d
Dakota J Pfeifer <dakota.pfeifer@yahoo.com>
2018-04-09 18:41:12 -0600

    fixed bug on video section. Now you can see whole face.

--- src/components/videos.js
+++ src/components/videos.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import aLeft from '../assets/leftarrow.png'
+import aRight from '../assets/rightarrow.png'
 
 class Videos extends Component{
   render(){
@@ -6,33 +8,50 @@ class Videos extends Component{
       <div id="videoArea">
         <div id="vidDisplay">
           <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
-            <h2 style={{marginBottom: 0+"em"}}>Programming</h2>
+            <h2 style={{marginBottom: 0+"em", margin: 1+"em"}}>Programming</h2>
             <a href="https://www.youtube.com/channel/UCqOyZalfSN_K5_eW3k7sAZw/videos?view_as=subscriber" target="_blank"><h3>View All Breakouts</h3></a>
           </div>
-          <div className="vids">
+          <div style={{display: "flex", alignItems: "center"}}>
             <div>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/xas71K-1AXI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              <img className="arrow" src={aLeft} alt="Left Arrow" />
+            </div>
+            <div className="vids">
+              <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/xas71K-1AXI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              </div>
+              <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/BKnQSDOcHV0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              </div>
+              <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/_ZlRqeAetJs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              </div>
             </div>
             <div>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/BKnQSDOcHV0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              <img className="arrow" src={aRight} alt="Right Arrow" />
             </div>
-            <div>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/_ZlRqeAetJs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            </div>
+
           </div>
           <div style={{display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
-            <h2 style={{marginBottom: 0+"em"}}>Creativity</h2>
+            <h2 style={{marginBottom: 0+"em", margin: 1+"em"}}>Creativity</h2>
             <a href="https://www.youtube.com/user/kh2guru/videos?view_as=subscriber" target="_blank"><h3>Doxyc Productions</h3></a>
           </div>
-          <div className="vids">
+          <div style={{display: "flex", alignItems: "center"}}>
             <div>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/B5mRak7FD9E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              <img className="arrow" src={aLeft} alt="Left Arrow" />
+            </div>
+            <div className="vids">
+              <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/B5mRak7FD9E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              </div>
+              <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/JebPTUBDxJ8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              </div>
+              <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/grTipPM73nM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              </div>
             </div>
             <div>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/JebPTUBDxJ8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-            </div>
-            <div>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/grTipPM73nM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              <img className="arrow" src={aRight} alt="Right Arrow" />
             </div>
           </div>
         </div>
