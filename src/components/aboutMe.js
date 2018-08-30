e8be5508e61e6ae4fff6947e9fad18704e4c29a1
Dakota <d-pfeif@outlook.com>
2018-08-29 19:29:24 -0600

    changed content

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -8,7 +8,6 @@ class AboutMe extends Component {
         <img alt="Dakota" src={myFace} className="myFace" />
         <div className="aboutSection">
           <h2>ABOUT ME</h2>
-          <p><i>Wheresoever you go, go with all your heart.</i> -Confucius</p>
           <p>I'm a full stack developer with a background in the medical field. My experience as a CNA has cultivated many soft skills that make me a successful developer: attention to details, team communication, time management, clear documentation, and critical thinking.</p>
           <p>I got my start into programming professionally through the Web Development Immersive at Galvanize in Denver, CO, but I've been dabbling since early high school. What I lack in experience I make up for with my hunger for knowledge, aptitude for quick learning, and notoriously positive attitude. I thrive as a team player; collaboration is one thing that propels me to put forth my best.</p>
           <p>Above all else, I live by the golden rule: do unto others as you would have them do unto you. I also believe in living honestly, respectfully, and passionately. I've always gone out of my way to help others in need. Those who know me would describe me as someone with a big heart, a substantial team player, reliable, and responsible.</p>
